7e22ceac4f98f2b4c3fac672b7376412bc753ce8
Kamran KB <thekamrankb@gmail.com>
2022-09-09 05:45:40 +0500

    Student portal layout

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,72 +1,36 @@
-<header class="mob-header-area">
-    <div class="mob-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-2 d-flex align-items-center">
-                    <a href="{{ route('front.home') }}" class="logo"><img src="{{ asset($brand_settings['logo']->key_value) }}"></a>
-                </div>
-                <button class="header-button"><i class="fas fa-bars"></i></button>
-                <div class="col-md-8">
-                    <div class="custom-collapse">
-                        <ul>
-                            <li> <a href="{{ route('front.home') }}" class="active">Home</a> </li>
-                            <li><a href="{{ route('front.about.us') }}">About Us</a></li>
-                            <li><a href="{{ route('front.logo.and.branding') }}">Logo & Branding</a> </li>
-                            <!--  <li class="dropdown"><a href="#">Services <i class="fas fa-angle-down carret"></i></a>
-                                <ul>
-                                        <li><a href="#">Logo Services</a></li>
-                                        <li><a href="#">Website Services</a></li>
-                                        <li><a href="#">Video Animation</a></li>
-                                </ul>
-                            </li> -->
-                            <li><a href="{{ route('front.website.design') }}">Website</a></li>
-                            <li><a href="{{ route('front.pricing') }}">Pricing</a></li>
-                            <li><a href="{{ route('front.portfolio') }}">Portfolio</a></li>
-                            <li><a href="{{ route('front.contact.us') }}">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </div>
-                @if(!empty($brand_settings["company_number"]->key_value))
-                    <div class="col-md-2 d-flex align-items-center">
-                        <a href="tel:{{ $brand_settings['company_number']->key_value }}" class="phone-1"><i class="fas fa-phone phone"></i>{{ $brand_settings["company_number"]->key_value }}</a>
-                    </div>
-                @endif
-            </div>
-        </div>
+<section class="panel-left-area">
+    <div class="panel-user-area">
+      <img src="{{asset('asset/images/user.jpg')}}" alt="User">
+      <p>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</p>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+      </form>
+      <a href="{{ route('logout') }}" onclick="event.preventDefault();
+      document.getElementById('logout-form').submit();">Logout</a>
     </div>
-</header>
-
-<header class="mob-menu">
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <a href="{{ route('front.home') }}" class="logo"><img src="{{ asset('frontend/assets/images/logo-2.png') }}" class="img-fluid"></a>
-            </div>
-            <div class="col-6 d-flex align-items-center justify-content-end">
-                <button class="header-button"><i class="fa-solid fa-bars"></i></button>
-            </div>
-            <div class="col-12 d-flex justify-content-start">
-                <div class="custom-collapse">
-                    <ul>
-                        <li> <a href="{{ route('front.home') }}">Home</a> </li>
-                        <li><a href="{{ route('front.home') }}">Logo & Branding</a> </li>
-                        <li><a href="{{ route('front.home') }}">About Us</a></li>
-                        <!--  <li class="dropdown"><a href="#">Services <i class="fas fa-angle-down carret"></i></a>
-                            <ul>
-                                    <li><a href="#">Logo Services</a></li>
-                                    <li><a href="#">Website Services</a></li>
-                                    <li><a href="#">Video Animation</a></li>
-                            </ul>
-                    </li> -->
-                        <li><a href="{{ route('front.home') }}">Website</a></li>
-                        <li><a href="{{ route('front.home') }}">Portfolio</a></li>
-                        <li><a href="{{ route('front.home') }}">Pricing</a></li>
-                        @if(!empty($brand_settings["company_number"]->key_value))
-                            <li><a href="tel:{{ $brand_settings['company_number']->key_value }}" class="phone-1"><i class="fas fa-phone phone"></i>{{ $brand_settings['company_number']->key_value }}</a></li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </div>
+    {{-- <div class="panel-tabs-area">
+      <h3>Teacher's Panel</h3>
+      <ul>
+        <li>
+          <a href="dashboard.html"><i class="fa-solid fa-list"></i>Students List</a>
+        </li>
+        <li>
+          <a href="viewTestQuestions.html"><i class="fa-solid fa-eye"></i>View Test Questions</a>
+        </li>
+        <li>
+          <a href="addTest.html"><i class="fa-solid fa-plus"></i>Add Test Question</a>
+        </li>
+      </ul>
+    </div> --}}
+    <div class="panel-tabs-area">
+      <h3>Student's Panel</h3>
+      <ul>
+        <li>
+          <a href="studentDashboard.html" class="active"><i class="fa-solid fa-graduation-cap"></i>Test Results</a>
+        </li>
+        <li>
+          <a href="takeTest.html"><i class="fa-solid fa-book"></i>Take A Test</a>
+        </li>
+      </ul>
     </div>
-</header>
+  </section>
