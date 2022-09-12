cb71ee4d930ac434b2623bf1775426bc4a660b1c
KamranKB <thekamrankb@gmail.com>
2022-09-12 17:04:57 +0500

    Added quizes function for listing purpose

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Contactinfo;
 use App\Models\Contactqueries;
 use App\Models\Pages;
+use App\Models\QuizStudentModel;
 use App\Models\Service;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Yajra\DataTables\Facades\DataTables;
 
 class FrontendController extends Controller
 {
@@ -15,7 +19,23 @@ class FrontendController extends Controller
         return view('frontend.pages.home');
     }
 
-    public function quizes() {
-        return view('frontend.pages.main');
+    public function quizes(Request $request) {
+        if ($request->ajax()) {
+            $quizes = QuizStudentModel::where('student_id', Auth::id())->with('student','quiz')->get();
+            return DataTables::of($quizes)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $html = '<button href="#"  class="btn btn-primary btn-view"  data-bs-toggle="modal" data-bs-target=".orderdetailsModal" data-id="' . $row->id . '"><i class="far fa-eye"></i></button>&nbsp';
+                    return $html;
+                })->addColumn('created_at', function ($row) {
+                    return date('d-M-Y', strtotime($row->created_at)) . '<br /> <label class="text-primary">' . Carbon::parse($row->created_at)->diffForHumans() . '</label>';
+                })->addColumn('status', function ($row) {
+                    $btn = '<div class="square-switch"><input type="checkbox" id="switch' . $row->id . '" class="banner_status" switch="bool" data-id="' . $row->id . '" value="' . ($row->active == 1 ? "1" : "0") . '" ' . ($row->active == 1 ? "checked" : "") . '/><label for="switch' . $row->id . '" data-on-label="Yes" data-off-label="No"></label></div>';
+
+                    return $btn;
+                })->rawColumns(['action', 'status', 'created_at'])->make(true);
+        }
+        
+        return view('frontend.pages.student_quiz');
     }
 }
