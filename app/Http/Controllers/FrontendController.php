5e6bc1f9fbfab6b34ee1c39ef01961f3b2303187
Kamran KB <thekamrankb@gmail.com>
2022-09-13 06:04:40 +0500

    Added pages for results and quiz

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -20,22 +20,12 @@ class FrontendController extends Controller
     }
 
     public function quizes(Request $request) {
-        if ($request->ajax()) {
-            $quizes = QuizStudentModel::where('student_id', Auth::id())->with('student','quiz')->get();
-            return DataTables::of($quizes)
-                ->addIndexColumn()
-                ->addColumn('action', function ($row) {
-                    $html = '<button href="#"  class="btn btn-primary btn-view"  data-bs-toggle="modal" data-bs-target=".orderdetailsModal" data-id="' . $row->id . '"><i class="far fa-eye"></i></button>&nbsp';
-                    return $html;
-                })->addColumn('created_at', function ($row) {
-                    return date('d-M-Y', strtotime($row->created_at)) . '<br /> <label class="text-primary">' . Carbon::parse($row->created_at)->diffForHumans() . '</label>';
-                })->addColumn('status', function ($row) {
-                    $btn = '<div class="square-switch"><input type="checkbox" id="switch' . $row->id . '" class="banner_status" switch="bool" data-id="' . $row->id . '" value="' . ($row->active == 1 ? "1" : "0") . '" ' . ($row->active == 1 ? "checked" : "") . '/><label for="switch' . $row->id . '" data-on-label="Yes" data-off-label="No"></label></div>';
-
-                    return $btn;
-                })->rawColumns(['action', 'status', 'created_at'])->make(true);
-        }
+        $quizes = QuizStudentModel::where('student_id', Auth::id())->with('student:id,first_name,last_name','quiz.subject:id,name')->get();
         
-        return view('frontend.pages.student_quiz');
+        return view('frontend.pages.student_quiz', compact('quizes'));
+    }
+
+    public function attempt_quiz(Request $request) {
+        return view('frontend.pages.quiz_attempt');
     }
 }
